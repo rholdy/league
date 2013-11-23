2d9bb93a6f2635b671f0586b3c4affb9452a8d0a
Alex Howington <alex.howington@gmail.com>
2013-11-23 11:21:16 -0700

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,14 @@ League::Application.routes.draw do
   end
   root :to => "home#index"
   devise_for :users
-  resources :users
+  resources :users do
+    resources :summoners
+  end
+  get "/users/:user_id/summoners(.:format)", to:"summoners#index", as: :summoners
+  post "/users/:user_id/summoners(.:format)", to: "summoners#create"
+  get "/users/:user_id/summoners/new(.:format)", to: "summoners#new", as: :new_summoner
+  get "/users/:user_id/summoners/:id/edit(.:format)", to: "summoners#edit", as: :edit_summoner
+  get "/users/:user_id/summoners/:id(.:format)", to: "summoners#show", as: :user_summoner
+  put "/users/:user_id/summoners/:id(.:format)", to: "summoners#update"
+  delete "/users/:user_id/summoners/:id(.:format)", to: "summoners#destroy"
 end
