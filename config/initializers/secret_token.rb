c82dc698355e8184b9caac256cb03bbace78d3f0
rholdy <rholdy@gmail.com>
2013-11-23 09:50:44 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+League::Application.config.secret_token = '2c1a9ffd969ca07307f439103a0d4ad6251fa3cf501368b174a43f6fe920e31a8ede1d660be5b65d7582a231ad557659520172cdeb58aa73ce078fa340ef8906'
