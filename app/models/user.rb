1d914947178b8d343d8e7803a768de0504394ad8
rholdy <rholdy@gmail.com>
2013-11-23 16:15:48 -0700

    Add Button Group on Show Summoner Page

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,7 @@ class User < ActiveRecord::Base
   attr_accessible :role_ids, :as => :admin
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
   has_many :summoners, dependent: :destroy
+  # validates :summoners, exclusion: { in: User.summoners.all,
+  #   message: "Can only add each summoner once." }
   
 end
